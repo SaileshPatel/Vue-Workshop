149f29bf1ebcbe985112fe3dd2b5034e1cc52add
Sailesh Patel <saileshapatel@gmail.com>
2019-02-16 22:28:40 +0000

    improve styling

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ Vue.component('form-component', {
         <form>
         <div class="form-row">
             <div class="form-group col-md-6">
-            <label for="inputEmail4">Email</label>
-            <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                <label for="inputEmail4">Email</label>
+                <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
             </div>
             <div class="form-group col-md-6">
-            <label for="inputPassword4">Password</label>
-            <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+                <label for="inputPassword4">Password</label>
+                <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
             </div>
         </div>
         <div class="form-group">
@@ -25,27 +25,27 @@ Vue.component('form-component', {
         </div>
         <div class="form-row">
             <div class="form-group col-md-6">
-            <label for="inputCity">City</label>
-            <input type="text" class="form-control" id="inputCity">
+                <label for="inputCity">City</label>
+                <input type="text" class="form-control" id="inputCity">
             </div>
             <div class="form-group col-md-4">
-            <label for="inputState">Region</label>
-            <select id="inputState" class="form-control">
-                <option selected>Choose...</option>
-                <option>...</option>
-            </select>
+                <label for="inputState">Region</label>
+                <select id="inputState" class="form-control">
+                    <option selected>Choose...</option>
+                    <option>...</option>
+                </select>
             </div>
             <div class="form-group col-md-2">
-            <label for="inputZip">Postcode</label>
-            <input type="text" class="form-control" id="inputZip">
+                <label for="inputZip">Postcode</label>
+                <input type="text" class="form-control" id="inputZip">
             </div>
         </div>
         <div class="form-group">
             <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="gridCheck">
-            <label class="form-check-label" for="gridCheck">
-                Check me out
-            </label>
+                <input class="form-check-input" type="checkbox" id="gridCheck">
+                <label class="form-check-label" for="gridCheck">
+                    Check me out
+                </label>
             </div>
         </div>
         <button type="submit" class="btn btn-primary">Sign in</button>
