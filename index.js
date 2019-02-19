f2e8ac0301ace685b258be4375722c63fceb5f6b
Sailesh Patel <saileshapatel@gmail.com>
2019-02-19 21:04:14 +0000

    replace address, city & postcode fields with form-input components

--- index.js
+++ index.js
@@ -25,18 +25,13 @@ Vue.component('form-component', {
             <form-input label-text='Password' form-type="password" column-size="col-md-6" element-id="inputPassword4" placeholder-text="Password"></form-input>
         </div>
         <div class="form-group">
-            <label for="inputAddress">Address</label>
-            <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+            <form-input label-text='Address' form-type="text" element-id="inputAddress" placeholder-text="1234 Main St"></form-input>
         </div>
         <div class="form-group">
-            <label for="inputAddress2">Address 2</label>
-            <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            <form-input label-text='Address 2' form-type="text" element-id="inputAddress2" placeholder-text="Apartment, studio, or floor"></form-input>
         </div>
         <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="inputCity">City</label>
-                <input type="text" class="form-control" id="inputCity">
-            </div>
+            <form-input label-text='City' form-type="text" column-size="col-md-6" element-id="inputCity"></form-input>
             <div class="form-group col-md-4">
                 <label for="inputState">Region</label>
                 <select id="inputState" class="form-control">
@@ -44,10 +39,7 @@ Vue.component('form-component', {
                     <option>...</option>
                 </select>
             </div>
-            <div class="form-group col-md-2">
-                <label for="inputZip">Postcode</label>
-                <input type="text" class="form-control" id="inputZip">
-            </div>
+            <form-input label-text='Postcode' form-type="text" column-size="col-md-2" element-id="inputZip"></form-input>
         </div>
         <div class="form-group">
             <div class="form-check">
