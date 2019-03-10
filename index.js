e638948a0551611157670f37ce633b647c333c3f
Sailesh Patel <saileshapatel@gmail.com>
2019-03-10 21:17:33 +0000

    create and use custom event

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 Vue.component('form-input', {
-    props: ['formType', 'columnSize', 'elementId', 'placeholderText', 'labelText'],
+    props: ['formType', 'columnSize', 'elementId', 'placeholderText', 'labelText', 'name'],
     data: function(){
         return {
             input: '',
@@ -8,9 +8,14 @@ Vue.component('form-input', {
     template: `
     <div class="form-group" v-bind:class="columnSize">
         <label v-bind:for="elementId">{{labelText}}</label>
-        <input v-model="input" v-bind:type="formType" class="form-control" v-bind:id="elementId" v-bind:placeholder="placeholderText">
+        <input v-model="input" v-bind:type="formType" class="form-control" v-bind:id="elementId" v-bind:placeholder="placeholderText" v-on:keypress="sendInformation();">
     </div>
-    `
+    `,
+    methods: {
+        sendInformation: function(){
+            this.$emit('send-form-input', {name: this.name, data: this.input});
+        }
+    }
 });
 
 Vue.component('selection-box', {
@@ -64,19 +69,19 @@ Vue.component('form-component', {
     template: `
         <form>
         <div class="form-row">
-            <form-input label-text='Email' form-type="email" column-size="col-md-6" element-id="inputEmail14" placeholder-text="Email"></form-input>
-            <form-input label-text='Password' form-type="password" column-size="col-md-6" element-id="inputPassword4" placeholder-text="Password"></form-input>
+            <form-input name="email" label-text='Email' form-type="email" column-size="col-md-6" element-id="inputEmail14" placeholder-text="Email"></form-input>
+            <form-input name="password" label-text='Password' form-type="password" column-size="col-md-6" element-id="inputPassword4" placeholder-text="Password"></form-input>
         </div>
         <div class="form-group">
-            <form-input label-text='Address' form-type="text" element-id="inputAddress" placeholder-text="1234 Main St"></form-input>
+            <form-input name="address" label-text='Address' form-type="text" element-id="inputAddress" placeholder-text="1234 Main St"></form-input>
         </div>
         <div class="form-group">
-            <form-input label-text='Address 2' form-type="text" element-id="inputAddress2" placeholder-text="Apartment, studio, or floor"></form-input>
+            <form-input name="address2" label-text='Address 2' form-type="text" element-id="inputAddress2" placeholder-text="Apartment, studio, or floor"></form-input>
         </div>
         <div class="form-row">
-            <form-input label-text='City' form-type="text" column-size="col-md-6" element-id="inputCity"></form-input>
+            <form-input name="city" label-text='City' form-type="text" column-size="col-md-6" element-id="inputCity"></form-input>
             <selection-box input-name="Region" column-size="col-md-4" v-bind:selection="['West Midlands', 'East Midlands']" element-id="inputState"></selection-box>
-            <form-input label-text='Postcode' form-type="text" column-size="col-md-2" element-id="inputZip"></form-input>
+            <form-input name="postcode" label-text='Postcode' form-type="text" column-size="col-md-2" element-id="inputZip"></form-input>
         </div>
         <div class="form-group">
             <check-box elementId="gridCheck" inputName="Check me out"></check-box>
